12154c4ef75259dd4b0e73252af100da739c08ef
Baptiste Régné <baptiste.regne@yahoo.fr>
2015-11-04 15:14:34 +0100

    next one

--- pages/1-7-reparti.php
+++ pages/1-7-reparti.php
@@ -1,45 +1,61 @@
-<section class="section" data-anchor="<?= $anchor ?>" data-background="bg-plitvice.jpg">
+<section class="section" data-anchor="<?= $anchor ?>" data-background="bg-krkra.jpg">
 
-	<div class="slide" data-anchor="slide1">
+	<div class="slide" data-anchor="souslorage">
 		<div class="slide-content">
-			<div class="container" style="background-color: rgba(17, 12, 8, .7);">
-	        <header>
-	            <h2>Hier</h2>
-	            <div class="stamp">
-	                <span><i class="icon-pencil"></i> Instantanné</span>
-	                <span><i class="icon-marker"></i> Zagreb, Croatie</span>
-	                <span><i class="icon-clock"></i> mai 2015</span>
-	            </div>
-	            <div class="icon-cancel"></div>
-	        </header>
-	        <div class="content column">
-	            <p>Je voyage depuis cinq jours sans me sentir dans l'aventure et fatalement, aujourd'hui, après avoir heurté cette voiture, j'y suis. J'ai vu un sacré paquet de choses défiler pendant mon roulé-boulé et depuis, j'ai fait des rencontres géniales : Antonio le sauveur, Miljenko le mécano, Dubravko motard aventurier aguerri...</p>
-	            <p>Mon voyage interpelle, de part ma volonté d'être un aventurier solidaire et solitaire. Leur gentillesse est exemplaire et je sais maintenant qu'on rencontre presque plus de monde lorsqu'on est en difficulté ou dans le besoin.</p>
-	            <p>Les motards forment une équipe soudée, j'en ai eu la preuve concrète hier. Il faut dire que vu l'état de la miss en me relevant, j'en avais besoin. Dans un an je rirais peut-être de cette histoire qui m’a beaucoup appris. J'ai une chance inouïe d'être en vie, de continuer ce voyage et de savoir que je peux compter sur  beaucoup de monde . J'espère poursuivre ce rêve et je vais (surtout le mécano) faire tout mon possible pour.</p>
-	            <p>Mais vais-je réussir à repartir, à trouver la force de continuer et surtout à retrouver confiance en moi? Je crois que le principal c'est d'y croire, j'y crois bien plus qu'hier et j'y croirai encore plus demain.</p>
-	       </div>
-	    </div>
+			<section>
+				<div class="container medium" style="background-color: rgba(17, 12, 8, .7);">
+		        <header>
+		            <h2>Sous l'orage</h2>
+		            <div class="stamp">
+		                <span><i class="icon-pencil"></i> Instantanné</span>
+		                <span><i class="icon-marker"></i> Sous un orage, Croatie</span>
+		                <span><i class="icon-clock"></i> mai 2015</span>
+		            </div>
+		            <div class="icon-cancel"></div>
+		        </header>
+		        <div class="content column">
+			        <p>Me voilà reparti mais cette fois je vais voyager différemment. Je manque d’expérience et les 4 voies ne sont pas faite pour moi. Je vais profiter des jours qui viennent pour appréhender plus tranquillement mon voyage. Je dois me sentir bien pour poursuivre et faire en sorte que les choses deviennent plus naturelles et agréables. J’ai évacué beaucoup de stress avec l’accident. Ça m’a donné des forces et une bonne raison de continuer l’aventure. J’y crois !</p>
+		       </div>
+		    </div>
+			</section>
 		</div>
 	</div>
 	
-  <div class="slide" data-anchor="slide1">
+  <div class="slide" data-anchor="antoniogrill">
 		<div class="slide-content">
-			<div class="container" style="background-color: rgba(17, 12, 8, .7);">
+			<div class="container small" style="background-color: rgba(17, 12, 8, .7);">
 	        <header>
-	            <h2>Hier</h2>
+	            <h2>Antonio Terasse Grill</h2>
 	            <div class="stamp">
 	                <span><i class="icon-pencil"></i> Instantanné</span>
-	                <span><i class="icon-marker"></i> Zagreb, Croatie</span>
+	                <span><i class="icon-marker"></i> Seger, Croatie</span>
 	                <span><i class="icon-clock"></i> mai 2015</span>
 	            </div>
 	            <div class="icon-cancel"></div>
 	        </header>
 	        <div class="content column">
-	            <p>Je voyage depuis cinq jours sans me sentir dans l'aventure et fatalement, aujourd'hui, après avoir heurté cette voiture, j'y suis. J'ai vu un sacré paquet de choses défiler pendant mon roulé-boulé et depuis, j'ai fait des rencontres géniales : Antonio le sauveur, Miljenko le mécano, Dubravko motard aventurier aguerri...</p>
-	            <p>Mon voyage interpelle, de part ma volonté d'être un aventurier solidaire et solitaire. Leur gentillesse est exemplaire et je sais maintenant qu'on rencontre presque plus de monde lorsqu'on est en difficulté ou dans le besoin.</p>
-	            <p>Les motards forment une équipe soudée, j'en ai eu la preuve concrète hier. Il faut dire que vu l'état de la miss en me relevant, j'en avais besoin. Dans un an je rirais peut-être de cette histoire qui m’a beaucoup appris. J'ai une chance inouïe d'être en vie, de continuer ce voyage et de savoir que je peux compter sur  beaucoup de monde . J'espère poursuivre ce rêve et je vais (surtout le mécano) faire tout mon possible pour.</p>
-	            <p>Mais vais-je réussir à repartir, à trouver la force de continuer et surtout à retrouver confiance en moi? Je crois que le principal c'est d'y croire, j'y crois bien plus qu'hier et j'y croirai encore plus demain.</p>
-	       </div>
+	        	<p>J’avais pas remarqué l’enseigne du restaurant au départ mais je me suis senti obligé de rester sachant qu’il porte le nom de mon sauveur croate. J’ai le pressentiment que quoi que je prenne, ce sera bon ! Aujourd’hui j’ai pris une claque entre les chutes Krkra, qui étaient magnifiques, le premier bain dans l’adriatique, et les nombreuses routes sinueuses. Une journée qui me réconforte et qui s’est terminé par une bière avec un groupe de français au camping. Demain je fixe quelques petites choses sur la belle et quand le linge sera sec je partirai pour le sud. Je pense atteindre Ankara dans une semaine.</p> 
+						<p>Le calamar était parfait !</p>
+	        </div>
+	    </div>
+		</div>
+	</div>
+
+	<div class="slide" data-anchor="motard">
+		<div class="slide-content">
+			<div class="container small" style="background-color: rgba(17, 12, 8, .7);">
+	        <header>
+	            <h2>Motard</h2>
+	            <div class="stamp">
+	                <span><i class="icon-pencil"></i> Instantanné</span>
+	                <span><i class="icon-marker"></i> Dubrovnik, Croatie</span>
+	                <span><i class="icon-clock"></i> mai 2015</span>
+	            </div>
+	            <div class="icon-cancel"></div>
+	        </header>
+	        <div class="content column">
+	        	<p>Peut-être mon dernier jour en Croatie. Bar jazzie avec petite bouffe sympa à la clé. Ça donne envie de rester même si le portefeuille en prend un coup. J’ai ma petite chambre au chaud chez un motard. Chaque jour est différent c’est génial. Quand je me lève j’ai toujours du mal mais une fois parti tout va bien. C’est bizarre cette sensation tous les matins mais je trouve mon rythme petit à petit.</p>
+	        </div>
 	    </div>
 		</div>
 	</div>
